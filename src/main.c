67859006d8aa96f04b31608db87b839bf17ff0da
Martin-Martinez4 <martmartez4@gmail.com>
2025-10-08 13:52:25 -0700

    Drew a rectangle with a line through it; still seems to work on my Windows machine

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_rect.h>
 #include <stdio.h>
+
+const int WINDOW_WIDTH = 900;
+const int WINDOW_HEIGHT = 506;
 
 int main(int argc, char* argv[]) {
 
@@ -12,8 +17,8 @@ int main(int argc, char* argv[]) {
     // Create an application window with the following settings:
     window = SDL_CreateWindow(
         "An SDL3 window",                  // window title
-        640,                               // width, in pixels
-        480,                               // height, in pixels
+        WINDOW_WIDTH,                               // width, in pixels
+        WINDOW_HEIGHT,                               // height, in pixels
         SDL_WINDOW_OPENGL                  // flags - see below
     );
 
@@ -30,6 +35,13 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // Drawn from top left corner it seems
+    SDL_FRect background;
+    background.x = (int)(WINDOW_WIDTH/4);
+    background.y = (int)(WINDOW_HEIGHT/4);
+    background.h =  (int)(WINDOW_HEIGHT/2);
+    background.w = (int)(WINDOW_WIDTH/2);
+
 
     while (!done) {
         SDL_Event event;
@@ -41,6 +53,22 @@ int main(int argc, char* argv[]) {
         }
 
         // Do game logic, present a frame, etc.
+
+        // fill with black background
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Draw filled off-white rectangle
+        SDL_SetRenderDrawColor(renderer, 250, 249, 246, 255);
+        int status = SDL_RenderFillRect(renderer, &background);
+
+        // Draw blue line
+         SDL_SetRenderDrawColor(renderer, 50, 50, 246, 255);
+        SDL_RenderLine(renderer, (int)(WINDOW_WIDTH/4), (int)(WINDOW_HEIGHT/4), (int)(WINDOW_WIDTH/4) + (int)(WINDOW_WIDTH/2), (int)(WINDOW_HEIGHT/4) + (int)(WINDOW_HEIGHT/2));
+
+
+        SDL_RenderPresent(renderer);
+
     }
 
     // Close and destroy the window
